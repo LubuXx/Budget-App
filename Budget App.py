ffb46bc7c1843e2c803d1170161edb180d79f7a8
Burak Baş <150476424+LubuXx@users.noreply.github.com>
2025-09-25 10:47:42 +0300

    Update Budget App.py

--- Budget App.py
+++ Budget App.py
@@ -1,7 +1,6 @@
 class Category:
     def __init__(self, name):
         self.name = name
-
         self.ledger = []
 
     def deposit(self, amount, description = ""):
@@ -10,7 +9,6 @@ class Category:
     def withdraw(self, amount, description = ""):
         if self.check_funds(amount):
             self.ledger.append({'amount': -amount, 'description': description})
-
             return True
         
         return False
@@ -21,9 +19,7 @@ class Category:
     def transfer(self, amount, category):
         if self.check_funds(amount):
             self.withdraw(amount, f"Transfer to {category.name}")
-
             category.deposit(amount, f"Transfer from {self.name}")
-
             return True
         
         return False
@@ -33,43 +29,33 @@ class Category:
     
     def __str__(self):
         title = f"{self.name: *^30}\n"
-
         items = ""
 
         for item in self.ledger:
             description = f"{item['description'][:23]:23}"
-
             amount = f"{item['amount']:>7.2f}"
-
             items += f"{description}{amount}\n"
 
         total = f"Total: {self.get_balance():.2f}"
-
         return title + items + total
     
 def create_spend_chart(categories):
     title = "Percentage spent by category"
-
     withdrawals = []
 
     for category in categories:
         total_withdrawals = sum(item['amount'] for item in category.ledger if item['amount'] < 0)
-
         withdrawals.append(total_withdrawals)
 
     total_withdrawals = sum(withdrawals)
-
     percentages = [(withdrawal / total_withdrawals) * 100 for withdrawal in withdrawals]
-
     rounded_percentages = [int(percent) // 10 * 10 for percent in percentages]
-
     chart = ""
 
     for i in range(100, -1, -10):
         chart += f"{i:>3}| " + " ".join("o" if percent >= i else " " for percent in rounded_percentages) + "  \n"
 
     chart += "    -" + "---" * len(categories) + "\n"
-
     max_len = max(len(category.name) for category in categories)
 
     for i in range(max_len):
